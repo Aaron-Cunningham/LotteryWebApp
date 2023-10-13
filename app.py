79208b9f8d47be13dcb091dc274de837946d04fe
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-10-13 17:39:01 +0100

    Added Error Handling

--- app.py
+++ app.py
@@ -31,5 +31,25 @@ app.register_blueprint(admin_blueprint)
 app.register_blueprint(lottery_blueprint)
 
 
+@app.errorhandler(400)
+def internal_error(error):
+    return render_template('errors/400.html'), 400
+
+@app.errorhandler(403)
+def internal_error(error):
+    return render_template('errors/403.html'), 403
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('errors/500.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/404.html'), 500
+
+@app.errorhandler(503)
+def internal_error(error):
+    return render_template('errors/404.html'), 503
+
+
 if __name__ == "__main__":
     app.run()
