9778e8842b41d94d95e2b9424a520938abbce6f8
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-10-23 17:11:07 +0100

    Added LoginForm and PasswordForm

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 import re
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+
+import pyotp
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, Email, ValidationError, EqualTo, Length
 
 
@@ -21,7 +23,7 @@ def password_check(form, field):
     p = re.compile(r'(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d])')
     if not p.match(field.data):
         raise ValidationError(
-            "Password must contain 1 digit, 1 lowercase letter, 1 uppercase letter and, 1 special character")
+            "Password must contain 1 digit, 1 lowercase letter, 1 uppercase letter and, 1 special character, and 6-12 characters in length")
 
 
 def postcode_check(form, field):
@@ -30,23 +32,41 @@ def postcode_check(form, field):
     z = re.compile('[A-Za-z][A-Za-z]\d\s\d[A-Za-z][A-Za-z]')
 
     if not (x.match(field.data) or y.match(field.data) or z.match(field.data)):
-        raise ValidationError("Postcode should be in these formats. (X: LETTERS Y: DIGITS with spaces) *XY YXX* *XYY YXX* *XXY YXX*")
-
+        raise ValidationError(
+            "Postcode should be in these formats. (X: LETTERS Y: DIGITS with spaces) *XY YXX* *XYY YXX* *XXY YXX*")
 
 
 def year_check(form, field):
     x = re.compile('\d{2}/\d{2}/\d{4}')
     if not x.match(field.data):
         raise ValidationError("Error in date of birth should be in this format DD/MM/YYYY")
+
+
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     firstname = StringField('First name', validators=[DataRequired(), character_check])
     lastname = StringField('Last name', validators=[DataRequired(), character_check])
     phone = StringField('Phone number', validators=[DataRequired(), phone_check])
     password = PasswordField('Password', validators=[DataRequired(), password_check, Length(min=6, max=12)])
-    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), Length(min=6, max=12),
-                                                                     EqualTo('password',
-                                                                             message='Passwords must match')])
+    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), Length(min=6, max=12), EqualTo('password', message='Passwords must match')])
     date_of_birth = StringField('Date of Birth', validators=[DataRequired(), year_check])
     post_code = StringField('Post Code', validators=[DataRequired(), postcode_check])
     submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    pin = StringField('Pin', validators=[DataRequired(), Length(min=6, max=6, message="Pin must be 6 digits")])
+    recaptcha = RecaptchaField()
+
+    submit = SubmitField()
+
+
+class PasswordForm(FlaskForm):
+    current_password = PasswordField(id='password', validators=[DataRequired()])
+    show_password = BooleanField('Show password', id='check')
+    new_password = PasswordField(validators=[DataRequired(), Length(min=6, max=12), password_check])
+    confirm_new_password = PasswordField(validators=[DataRequired(), EqualTo('new_password', message='Passwords must match')])
+    submit = SubmitField('Change Password')
