0bc80b05067dd92f99fa8f38a829fc98a3bcb9dc
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-11-01 00:15:33 +0000

    -Added logger and file handler

--- app.py
+++ app.py
@@ -1,25 +1,41 @@
 # IMPORTS
+import logging
+import os
 from functools import wraps
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from flask_qrcode import QRcode
 
-
-
 # CONFIG
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lottery.db'
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lcqli4oAAAAABDYGdAO_ULvasA2XLWGTHEuDJjx'
-app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lcqli4oAAAAAOKLDTKBslSdHS8Woqx4RGVa2yYH'
-
+app.config['SQLALCHEMY_ECHO'] = os.getenv('SQLALCHEMY_ECHO') == True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') == True
+app.config['RECAPTCHA_PUBLIC_KEY'] = os.getenv('RECAPTCHA_PUBLIC_KEY')
+app.config['RECAPTCHA_PRIVATE_KEY'] = os.getenv('RECAPTCHA_PRIVATE_KEY')
 # initialise database
 db = SQLAlchemy(app)
 qrcode = QRcode(app)
+
+
+# Logging
+class SecurityFilter(logging.Filter):
+    def filter(self, record):
+        return 'SECURITY' in record.getMessage()
+
+
+file_handler = logging.FileHandler('lottery.log', 'a')
+file_handler.setLevel(logging.WARNING)
+file_handler.addFilter(SecurityFilter())
+formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(file_handler)
+
 
 # HOME PAGE VIEW
 @app.route('/')
@@ -27,15 +43,22 @@ def index():
     return render_template('main/index.html')
 
 
+# Role checking
 def requires_roles(*roles):
     def wrapper(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
             if current_user.role not in roles:
+                # Sends a log to the log file if there was an unauthorised access attempts
+                logging.warning('SECURITY - Unauthorised Access Attempts [%s, %s, %s, %s]', current_user.id,
+                                current_user.email, current_user.role, request.remote_addr)
                 return render_template('errors/403.html')
             return f(*args, **kwargs)
+
         return wrapped
+
     return wrapper
+
 
 # BLUEPRINTS
 # import blueprints
@@ -49,6 +72,7 @@ app.register_blueprint(users_blueprint)
 app.register_blueprint(admin_blueprint)
 app.register_blueprint(lottery_blueprint)
 
+# Login manager
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.init_app(app)
@@ -59,8 +83,6 @@ from models import User
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
-
 
 
 @app.errorhandler(400)
@@ -90,5 +112,3 @@ def internal_error(error):
 
 if __name__ == "__main__":
     app.run()
-
-
