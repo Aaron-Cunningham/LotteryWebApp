22d97ce57d0d6a0f4e64da48a010fe9c251ead3d
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-11-20 23:11:53 +0000

    -Added img src to Context security header -Fixed bugs with internal_errors and renamed page errors

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import logging
 import os
 from functools import wraps
+from dotenv import load_dotenv
 from flask_talisman import Talisman
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
@@ -10,8 +11,9 @@ from flask_qrcode import QRcode
 
 # CONFIG
 app = Flask(__name__)
+load_dotenv()
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lottery.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_ECHO'] = os.getenv('SQLALCHEMY_ECHO') == True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') == True
 app.config['RECAPTCHA_PUBLIC_KEY'] = os.getenv('RECAPTCHA_PUBLIC_KEY')
@@ -20,9 +22,8 @@ app.config['RECAPTCHA_PRIVATE_KEY'] = os.getenv('RECAPTCHA_PRIVATE_KEY')
 db = SQLAlchemy(app)
 # initialise security headers
 
-# initialise QR code
-qrcode = QRcode(app)
-# Security header
+
+# content security header
 csp = {
     'default-src': [
         '\'self\'',
@@ -38,24 +39,16 @@ csp = {
         '\'unsafe-inline\'',
         'https://www.google.com/recaptcha/',
         'https://www.gstatic.com/recaptcha/'
+    ],
+    'img-src': [
+        'data:'
     ]
+
 }
 talisman = Talisman(app, content_security_policy=csp)
 
-# Logging
-class SecurityFilter(logging.Filter):
-    def filter(self, record):
-        return 'SECURITY' in record.getMessage()
-
-# File handler setup
-file_handler = logging.FileHandler('lottery.log', 'a')
-file_handler.setLevel(logging.WARNING)
-file_handler.addFilter(SecurityFilter())
-formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
-file_handler.setFormatter(formatter)
-
-logger = logging.getLogger()
-logger.addHandler(file_handler)
+# initialise QR code
+qrcode = QRcode(app)
 
 
 # HOME PAGE VIEW
@@ -87,7 +80,6 @@ from users.views import users_blueprint
 from admin.views import admin_blueprint
 from lottery.views import lottery_blueprint
 
-
 # # register blueprints with app
 app.register_blueprint(users_blueprint)
 app.register_blueprint(admin_blueprint)
@@ -100,29 +92,55 @@ login_manager.init_app(app)
 
 from models import User
 
+
 # Searches database for a user with same ID
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+
 # Error pages
 @app.errorhandler(400)
-def internal_error(error):
+def bad_request(error):
     return render_template('errors/400.html'), 400
+
+
 @app.errorhandler(403)
-def internal_error(error):
+def forbidden_error(error):
     return render_template('errors/403.html'), 403
+
+
 @app.errorhandler(404)
-def internal_error(error):
-    return render_template('errors/500.html'), 404
+def page_not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('errors/404.html'), 500
+    return render_template('errors/500.html'), 500
+
+
 @app.errorhandler(503)
 def internal_error(error):
-    return render_template('errors/404.html'), 503
+    return render_template('errors/503.html'), 503
 
+
+# Logging
+class SecurityFilter(logging.Filter):
+    def filter(self, record):
+        return 'SECURITY' in record.getMessage()
+
+
+# File handler setup
+file_handler = logging.FileHandler('lottery.log', 'a')
+file_handler.setLevel(logging.WARNING)
+file_handler.addFilter(SecurityFilter())
+formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(file_handler)
 
 if __name__ == "__main__":
-    # If doesn't work add '--cert=cert.pem --key=key.pem' in Edit configuration/Additional options
+    # If it doesn't work add '--cert=cert.pem --key=key.pem' in Edit configuration/Additional options
     app.run(ssl_context=('cert.pem', 'key.pem'))
