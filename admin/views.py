2bd08c058a0d883de62bfe32a21e71887e9df56d
Aaron-Cunningham <aaroncunningham222@gmail.com>
2023-12-20 01:34:46 +0000

    enabled admin features

--- admin/views.py
+++ admin/views.py
@@ -13,6 +13,9 @@ admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 
 # VIEWS
 # view admin homepage
+@admin_blueprint.route('/admin')
+@login_required
+@requires_roles('admin')
 def admin():
     # Only allows admins to access the page
 
@@ -20,7 +23,10 @@ def admin():
 
 
 # create a new winning draw
-
+@admin_blueprint.route('/generate_winning_draw', methods=['POST'])
+@admin_blueprint.route('/admin')
+@login_required
+@requires_roles('admin')
 def generate_winning_draw():
     # get current winning draw
     current_winning_draw = Draw.query.filter_by(master_draw=True).first()
@@ -57,7 +63,10 @@ def generate_winning_draw():
 
 
 # view current winning draw
-
+@admin_blueprint.route('/view_winning_draw', methods=['POST'])
+@admin_blueprint.route('/admin')
+@login_required
+@requires_roles('admin')
 def view_winning_draw():
     # get winning draw from DB
     current_winning_draw = Draw.query.filter_by(master_draw=True, been_played=False).first()
@@ -78,7 +87,10 @@ def view_winning_draw():
 
 
 # view lottery results and winners
-
+@admin_blueprint.route('/run_lottery', methods=['POST'])
+@admin_blueprint.route('/admin')
+@login_required
+@requires_roles('admin')
 def run_lottery():
     # get current unplayed winning draw
     current_winning_draw = Draw.query.filter_by(master_draw=True, been_played=False).first()
@@ -142,14 +154,20 @@ def run_lottery():
 
 
 # view all registered users
-
+@admin_blueprint.route('/view_all_users', methods=['POST'])
+@admin_blueprint.route('/admin')
+@login_required
+@requires_roles('admin')
 def view_all_users():
     current_users = User.query.filter_by(role='user').all()
 
     return render_template('admin/admin.html', name=current_user.firstname, current_users=current_users)
 
 
-
+@admin_blueprint.route('/view_user_activity', methods=['POST'])
+@admin_blueprint.route('/admin')
+@login_required
+@requires_roles('admin')
 def view_user_activity():
     current_users_activity = User.query.filter_by(role='user').all()
 
